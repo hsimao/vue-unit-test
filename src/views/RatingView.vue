be1d7af0dd39ef6f6af8c7c3f2d9e57a0f855976
hsimao <e087754958@gmail.com>
2020-04-19 20:59:58 +0800

    feat: Rating component test

---
+++ src/views/RatingView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="RatingView">
+    <Rating :grade="2" />
+  </div>
+</template>
+
+<script>
+import Rating from '@/components/Rating'
+export default {
+  name: 'RatingView',
+  components: {
+    Rating
+  }
+}
+</script>
+
+<style>
+html,
+body,
+#RatingView {
+  height: 100%;
+}
+#RatingView {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f5f5f5;
+}
+</style>
---
+++ tests/unit/Rating.spec.js
@@ -0,0 +1,84 @@
+import Rating from '@/components/Rating'
+import { shallowMount } from '@vue/test-utils'
+
+let wrapper = null
+
+beforeEach(() => {
+  wrapper = shallowMount(Rating, {
+    propsData: {
+      maxStars: 6,
+      grade: 3
+    }
+  })
+})
+
+afterEach(() => {
+  wrapper.destroy()
+})
+
+describe('Rating', () => {
+  it('renders the stars', () => {
+    const stars = wrapper.findAll('.star')
+
+    expect(stars.length).toBe(6)
+  })
+
+  it('renders the active stars', () => {
+    const active = wrapper.findAll('.star.active')
+
+    expect(active.length).toBe(3)
+  })
+
+  it('renders a counter', () => {
+    expect(wrapper.find('[data-test-id="counter"]').exists()).toBe(true)
+  })
+
+  it('displays how many stars are active', () => {
+    expect(wrapper.find('[data-test-id="counter"]').text()).toMatch('3 of 6')
+  })
+
+  it('renders a list of stars without class `active` on remaining stars', () => {
+    expect(
+      wrapper.findAll('[data-test-id="star"]').length -
+        wrapper.findAll('.active').length
+    ).toEqual(3)
+  })
+
+  it('adds `active` class on an inactive star when the user clicks it', async () => {
+    const fourthStar = wrapper.findAll('[data-test-id="star"]').at(3)
+    fourthStar.trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(fourthStar.classes()).toContain('active')
+  })
+
+  it('sets icon to `star` in an inactive star when the user clicks it', async () => {
+    const fourthStar = wrapper.findAll('[data-test-id="star"]').at(3)
+    fourthStar.trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(fourthStar.find('[data-test-id="icon"]').attributes().name).toEqual(
+      'star'
+    )
+  })
+
+  it('removes `active` class on an active star when the user clicks it', async () => {
+    const thirdStar = wrapper.findAll('[data-test-id="star"]').at(2)
+    thirdStar.trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(thirdStar.classes()).not.toContain('active')
+  })
+
+  it('sets icon to `regular/star` in an inactive star when the user clicks it', async () => {
+    const thirdStar = wrapper.findAll('[data-test-id="star"]').at(2)
+    thirdStar.trigger('click')
+    await wrapper.vm.$nextTick()
+    expect(thirdStar.find('[data-test-id="icon"]').attributes().name).toEqual(
+      'regular/star'
+    )
+  })
+
+  it('does not render a counter if prop.hasCounter is set to false', async () => {
+    wrapper.setProps({ hasCounter: false })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('[data-test-id="counter"]').exists()).toBe(false)
+  })
+})
