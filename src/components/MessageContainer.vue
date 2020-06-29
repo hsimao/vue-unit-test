b24e58e5382bf3e3c6b6a5ccbe172bb46737877c
hsimao <e087754958@gmail.com>
2020-06-29 20:04:44 +0800

    feat: use stubs test child component

---
+++ src/components/MessageContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <MessageDisplay />
+  </div>
+</template>
+
+<script>
+import MessageDisplay from '@/components/MessageDisplay'
+
+export default {
+  name: 'MessageContainer',
+  components: {
+    MessageDisplay
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
